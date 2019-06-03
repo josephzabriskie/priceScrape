37cc4946016a02b658bd9b09701f024e645da4c6
jzabriskie <jzabriskie@cradlepoint.com>
2019-06-03 17:02:48 -0600

    Initial commit

---
+++ scraper.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <curl/curl.h>
+
+//Idea: This app is a price monitoring program. You feed it a list of links to websites that are supported and it keeps an eye on price changes
+//  Can run at specified intervals and alerts to email given price increase/decrease
+using namespace std;
+
+int main(){
+    cout << "Welome to my crappy webscraper" << endl;
+    CURL *curl;
+    CURLcode res;
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+    curl = curl_easy_init();
+    if(curl){
+        curl_easy_setopt(curl, CURLOPT_URL, "https://www.google.com");
+        cout << "Set options" << endl;
+        res = curl_easy_perform(curl);
+        cout << "Performed" << endl;
+        if(res != CURLE_OK){
+            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+        }
+
+        //Final cleanup
+        curl_easy_cleanup(curl);
+    }
+    curl_global_cleanup();
+    return 0;
+}
